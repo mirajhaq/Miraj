60ae07d5e20b2594c7bd70485f5cc0cef70908b7
mirajhaq <151921265+mirajhaq@users.noreply.github.com>
2024-08-24 21:41:32 +0100

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/Intro.css";
 import EmailRoundedIcon from "@mui/icons-material//EmailRounded";
-import FractalTree from "./FractalTree";
+
 
 class Intro extends React.Component {
   constructor() {
@@ -21,7 +21,6 @@ class Intro extends React.Component {
   render() {
     return (
       <div id="intro">
-        <FractalTree></FractalTree>
         <div className="typist" avgTypingDelay={120}>
           <span className="intro-title">
             {"hi, "}
