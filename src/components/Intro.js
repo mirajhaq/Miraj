536eb142f8dc0dc3b7046fe4647fb007c15d1dc0
mirajhaq <151921265+mirajhaq@users.noreply.github.com>
2024-08-24 18:41:31 +0100

    Add files via upload

---
+++ src/components/Intro.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "../styles/Intro.css";
+import EmailRoundedIcon from "@mui/icons-material//EmailRounded";
+import FractalTree from "./FractalTree";
+
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+      visible: true,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    return (
+      <div id="intro">
+
+        <div className="typist" avgTypingDelay={120}>
+          <span className="intro-title">
+            {"hi, "}
+            <span className="intro-name">{"miraj"}</span>
+            {" here."}
+          </span>
+        </div>
+        <div>
+          <div className="intro-subtitle">I don't know what to do.</div>
+          <div className="intro-desc">
+            I'm a guy from London.
+          </div>
+          <a
+            href="mailto:mirajhaq@hotmail.com"
+            className="intro-contact"
+          >
+            <EmailRoundedIcon></EmailRoundedIcon>
+            {" Say hi!"}
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Intro;
