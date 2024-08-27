59bbd1c4d0f70393aa959b8b7e3cc177ea0da295
mirajhaq <151921265+mirajhaq@users.noreply.github.com>
2024-08-27 12:06:29 +0100

    Update FadeInSection.js

--- src/components/FadeInSection.js
+++ src/components/FadeInSection.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function FadeInSection(props) {
   const [isVisible, setVisible] = React.useState(false);
   const domRef = React.useRef();
+
   React.useEffect(() => {
+    const currentRef = domRef.current; // Store the current ref in a variable
+
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -11,9 +14,18 @@ export default function FadeInSection(props) {
         }
       });
     });
-    observer.observe(domRef.current);
-    return () => observer.unobserve(domRef.current);
-  }, []);
+
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, []); // Empty dependency array means this effect runs only on mount and unmount
+
   return (
     <div
       className={`fade-in-section ${isVisible ? "is-visible" : ""}`}
