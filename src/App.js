575b0d51f59078d08a13f4f241a7421ee7465761
mirajhaq <151921265+mirajhaq@users.noreply.github.com>
2024-08-27 11:51:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Intro from "./components/Intro";
 import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Credits from "./components/Credits";
 
 import './App.css';
 import "./styles/Global.css";
@@ -14,10 +17,13 @@ function App() {
       <div id="content">
         <Intro></Intro>
         <About></About>
-
+        <Experience></Experience>
+        <Projects></Projects>
+        <Credits></Credits>
       </div>
     </div>
   );
 }
 
 export default App;
+
