114f75a0cdfed78410226375c081de552aba6ad0
mirajhaq <151921265+mirajhaq@users.noreply.github.com>
2024-08-27 10:07:30 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,33 @@
 import React from "react";
 import "../styles/About.css";
 
-
 class About extends React.Component {
   constructor() {
     super();
     this.state = {
       expanded: true,
-      activeKey: "1"
+      activeKey: "1",
     };
     this.handleSelect = this.handleSelect.bind(this);
   }
+
   handleSelect(eventKey) {
     this.setState({
-      activeKey: eventKey
+      activeKey: eventKey,
     });
   }
+
   render() {
     const one = (
       <p>
         I am currently an <b>IT Analyst</b> at
-        <a href="https://www.revcap.co.uk/"> Revcap</a>, I am passionate about advancing my career in 
-        Systems Administration and cloud technologies, having completed the Azure Fundamentals certification 
-        and actively pursuing further expertise in Linux and AWS.
+        <a href="https://www.revcap.co.uk/"> Revcap</a>, I am passionate about
+        advancing my career in Systems Administration and cloud technologies,
+        having completed the Azure Fundamentals certification and actively
+        pursuing further expertise in Linux and AWS.
       </p>
     );
+
     const two = (
       <p>
         Outside of work, I'm interested in following the developments of
@@ -32,36 +35,27 @@ class About extends React.Component {
       </p>
     );
 
-    const tech_stack = [
-      "Typescript",
-      "Python",
-      "React.js",
-      "Java",
-      "Javascript ES6+",
-      "C#"
-    ];
-
     return (
       <div id="about">
         <div>
-          <div className="section-header ">
+          <div className="section-header">
             <span className="section-title">/ about me</span>
           </div>
           <div className="about-content">
-            //<div className="about-description">
-              //{[one]}
-              //{"Here are some technologies I have been working with:"}
-              //<ul className="tech-stack">
-               // {tech_stack.map(function (tech_item, i) {
-                  //return (
-                    //<div delay={`${i + 1}00ms`}>
-                     // <li>{tech_item}</li>
-                   // </div>
+            <div className="about-description">
+              {one}
+              {/* If you want to reintroduce the tech stack, uncomment this section */}
+              {/* <ul className="tech-stack">
+                {tech_stack.map(function (tech_item, i) {
+                  return (
+                    <div key={i} delay={`${i + 1}00ms`}>
+                      <li>{tech_item}</li>
+                    </div>
                   );
                 })}
-              </ul>
-              {[two]}
-            //</div>
+              </ul> */}
+              {two}
+            </div>
             <div className="about-image">
               <img alt="Miraj Haq" src={"/assets/me2.png"} />
             </div>
@@ -73,3 +67,4 @@ class About extends React.Component {
 }
 
 export default About;
+
