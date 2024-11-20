1f303101e06e2a14115e8966efeeaa6cbdb69c9d
mirajhaq <151921265+mirajhaq@users.noreply.github.com>
2024-11-20 17:35:00 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import ProjectDetails from "./components/ProjectDetails";
 import Credits from "./components/Credits";
 
 import './App.css';
 import "./styles/Global.css";
 
-
 function App() {
   return (
-    <div className="App">
-      <NavBar></NavBar>
-      <div id="content">
-        <About></About>
-        <Experience></Experience>
-        <Projects></Projects>
-        <Credits></Credits>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Home Route: Show NavBar and all sections */}
+          <Route
+            path="/"
+            element={
+              <>
+                <NavBar />
+                <div id="content">
+                  <About />
+                  <Experience />
+                  <Projects />
+                  <Credits />
+                </div>
+              </>
+            }
+          />
+
+          {/* Project Details Route: No NavBar, only Name */}
+          <Route path="/projects/:projectName" element={<ProjectDetails />} />
+
+          {/* Add routes for individual projects here */}
+          <Route
+            path="/projects/password-checker"
+            element={<ProjectDetails projectName="Password Checker" />}
+          />
+          <Route
+            path="/projects/ambient-lighting"
+            element={<ProjectDetails projectName="Ambient Lighting" />}
+          />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
-
