905256a75817ab39d902b1e396c3f5844be68453
mirajhaq <151921265+mirajhaq@users.noreply.github.com>
2024-11-21 14:58:59 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
 import Experience from "./components/Experience";
